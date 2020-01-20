9a543d20438d4baaf7877cf3fd3855def6824a98
aminedhobb <amine.dhobb@gmail.com>
2020-01-20 20:03:38 +0100

    toggle todos

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,14 @@
 export default function reducer(state, action) {
   switch(action.type) {
+
+    case 'TOGGLE_TODO':
+      const toggleTodos = state.todos.map(t =>
+        t.id === action.payload.id
+          ? { ...action.payload, complete: !action.payload.complete }
+          : t
+      )
+      return { ...state, todos: toggleTodos }
+
     default:
       return state
   }
